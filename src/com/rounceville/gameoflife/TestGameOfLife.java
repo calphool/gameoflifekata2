8fa778d5f78f49454536d03a783ccbb0509225db
calphool <jrounceville@hotmail.com>
2016-07-13 12:14:33 -0500

    live cell with 2 or 3 live nbrs test 2

--- src/com/rounceville/gameoflife/TestGameOfLife.java
+++ src/com/rounceville/gameoflife/TestGameOfLife.java
@@ -114,5 +114,19 @@ public class TestGameOfLife {
 		assertEquals(true,  gm2.getAt(1, 0));
 		assertEquals(false, gm2.getAt(2, 0));
 	}
+
+	@Test
+	public void testAnyLiveCellWithTwoOrThreeLiveNeighborsLivesOnToTheNextGenerationTest2() {
+		GridMatrix gm = new GridMatrix(3,4);
+		gm.setAt(0, 0, true);	
+		gm.setAt(1, 0, true);
+		gm.setAt(2, 0, true);
+		gm.setAt(1, 1, true);		
+		GridMatrix gm2 = GameOfLife.nextGeneration(gm);
+		assertEquals(true , gm2.getAt(0, 0));
+		assertEquals(true,  gm2.getAt(1, 0));
+		assertEquals(true, gm2.getAt(2, 0));
+		assertEquals(true, gm2.getAt(1, 1));		
+	}
 	
 }
