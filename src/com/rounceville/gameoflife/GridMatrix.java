f1e048e427e1c5d2cb9b2d251718c1dcaf4437b1
calphool <jrounceville@hotmail.com>
2016-07-13 09:28:08 -0500

    grid is addressable tests

--- src/com/rounceville/gameoflife/GridMatrix.java
+++ src/com/rounceville/gameoflife/GridMatrix.java
@@ -1,13 +1,19 @@
 package com.rounceville.gameoflife;
 
+import java.util.BitSet;
+
 public class GridMatrix {
 
+	private BitSet[] bsRowArray;
 	int iMyWidth = 0;
 	int iMyHeight = 0;
 	
 	public GridMatrix(int iWidth, int iHeight) {
 		iMyWidth = iWidth;
 		iMyHeight = iHeight;
+		bsRowArray = new BitSet[iHeight];
+		for (int i = 0; i < iHeight; i++)
+			bsRowArray[i] = new BitSet(iWidth);		
 	}
 
 	public int getWidth() {
@@ -15,6 +21,18 @@ public class GridMatrix {
 	}
 
 	public int getHeight() {
-		return iMyHeight;	}
+		return iMyHeight;	
+	}
+
+	public void setAt(int x, int y, boolean b) {
+		if(b)
+			bsRowArray[y].set(x);
+		else
+			bsRowArray[y].clear(x);
+	}
+
+	public boolean getAt(int x, int y) {
+		return bsRowArray[y].get(x);
+	}
 
 }
--- src/com/rounceville/gameoflife/TestGridMatrix.java
+++ src/com/rounceville/gameoflife/TestGridMatrix.java
@@ -18,6 +18,19 @@ public class TestGridMatrix {
 		assertEquals(3, gm.getWidth());
 		assertEquals(4, gm.getHeight());		
 	}
+
+	@Test
+	public void testGridHasAddressability() {
+		GridMatrix gm = new GridMatrix(3,4);
+		gm.setAt(0,0,false);
+		assertEquals(false, gm.getAt(0,0));
+		gm.setAt(0,0, true);
+		assertEquals(true, gm.getAt(0,0));
+		assertEquals(false, gm.getAt(2,3));		
+		gm.setAt(2,3, true);
+		assertEquals(true, gm.getAt(2,3));		
+	}
+
 	
 
 }
